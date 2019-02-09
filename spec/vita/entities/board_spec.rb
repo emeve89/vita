8a41b9851b5d30bb7020c9b688e1b8c7ec86a61e
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2019-02-09 15:24:48 +0100

    MOdify specs

--- spec/vita/entities/board_spec.rb
+++ spec/vita/entities/board_spec.rb
@@ -6,10 +6,9 @@ RSpec.describe Vita::Entities::Board do
   it { should respond_to(:state=) }
 
   describe '#state' do
-    it 'returns an array of arrays of Vita::Entities::Cell' do
+    it 'returns an array of Vita::Entities::Cell' do
       expect(subject.state.class).to eq Array
-      expect(subject.state.map(&:class).uniq).to eq [Array]
-      expect(subject.state.first.map(&:class).uniq).to eq [Vita::Entities::Cell]
+      expect(subject.state.map(&:class).uniq).to eq [Vita::Entities::Cell]
     end
   end
 
--- spec/vita/use_cases/game/start_spec.rb
+++ spec/vita/use_cases/game/start_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Vita::UseCases::Game::Start do
 
   it 'creates a Vita::Entities::Board' do
     expect(Vita::Entities::Board).to receive(:new)
-    described_class.new.call
+    described_class.new
   end
 
   it 'calls Vita::UseCases::Board::NextTick' do
-    allow(described_class).to receive(:loop).and_yield
+    allow_any_instance_of(described_class).to receive(:loop).and_yield
     expect(Vita::UseCases::Board::NextTick).to receive_message_chain(:new, :call)
     described_class.new.call
   end
