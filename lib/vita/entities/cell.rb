73c0dc63192093b0adae0a7b354c683de0a68da8
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2019-02-09 12:37:42 +0100

    Update cell entity

--- lib/vita/entities/cell.rb
+++ lib/vita/entities/cell.rb
@@ -1,12 +1,18 @@
 module Vita
   module Entities
     class Cell
-      STATUSES = %w(ALIVE DEAD)
+      STATUSES = %w(ALIVE DEAD).freeze
 
-      attr_accessor :state
+      attr_reader :state
 
       def initialize
         @state = STATUSES.sample
+      end
+
+      def set_state(state)
+        raise ArgumentError.new('state is invalid') unless STATUSES.include?(state)
+        @state = state
+        self
       end
     end
   end
