b69a6debbb4d28025dc337d401ca91e3dd68b554
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2019-02-09 12:40:47 +0100

    update spec

--- spec/vita/use_cases/cell/apply_rules_spec.rb
+++ spec/vita/use_cases/cell/apply_rules_spec.rb
@@ -5,12 +5,13 @@ RSpec.describe Vita::UseCases::Cell::ApplyRules do
 
   describe "#call" do
     it "returns a new Vita::Entities::Cell" do
-      expect(use_case.call(cell: double("cell"), alive_neighbors: 1).class).to eq Vita::Entities::Cell
+      cell = Vita::Entities::Cell.new.set_state('ALIVE')
+      expect(use_case.call(cell: cell, alive_neighbors: 1).class).to eq Vita::Entities::Cell
     end
 
     describe "first rule" do
       it "Any live cell with fewer than two live neighbors dies, as if by underpopulation." do
-        cell = double("alive cell", state: 'ALIVE')
+        cell = Vita::Entities::Cell.new.set_state('ALIVE')
         result = use_case.call(cell: cell, alive_neighbors: 1)
         expect(result.state).to eq 'DEAD'
 
@@ -21,7 +22,7 @@ RSpec.describe Vita::UseCases::Cell::ApplyRules do
 
     describe "second rule" do
       it "Any live cell with two or three live neighbors lives on to the next generation." do
-        cell = double("alive cell", state: 'ALIVE')
+        cell = Vita::Entities::Cell.new.set_state('ALIVE')
         [2, 3].each do |alive_neighbors|
           result = use_case.call(cell: cell, alive_neighbors: alive_neighbors)
           expect(result.state).to eq 'ALIVE'
@@ -31,8 +32,8 @@ RSpec.describe Vita::UseCases::Cell::ApplyRules do
 
     describe "third rule" do
       it "Any live cell with more than three live neighbors dies, as if by overpopulation." do
-        cell = double("alive cell", state: 'ALIVE')
-        [4...8].each do |alive_neighbors|
+        cell = Vita::Entities::Cell.new.set_state('ALIVE')
+        (4...8).each do |alive_neighbors|
           result = use_case.call(cell: cell, alive_neighbors: alive_neighbors)
           expect(result.state).to eq 'DEAD'
         end
@@ -41,7 +42,7 @@ RSpec.describe Vita::UseCases::Cell::ApplyRules do
 
     describe "fourth rule" do
       it "Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction." do
-        cell = double("dead cell", state: 'DEAD')
+        cell = Vita::Entities::Cell.new.set_state('DEAD')
         result = use_case.call(cell: cell, alive_neighbors: 3)
         expect(result.state).to eq 'ALIVE'
       end
